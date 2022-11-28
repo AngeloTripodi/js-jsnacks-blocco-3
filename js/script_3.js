94416ce7f932bd7f300fd60fb833957aafccf062
AngeloTripodi <tripodi.angelo@gmail.com>
2022-11-28 17:29:13 +0100

    add If

--- js/script_3.js
+++ js/script_3.js
@@ -1,7 +1,7 @@
 const array1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-const array2 = [2, 0, 3, 0, 3, 0, 0, 8, 99, 0, 18, 189, 42]
+const array2 = [2, 0, 3, 0, 3, 0, 0, 8, 99, 0, 18, 189, 42, 0, 33, 44, 88, 99]
 
 while (array1.length < array2.length) {
-    array1.push('0');
+    array1.push(0);
 }
 console.log(array1, array2);

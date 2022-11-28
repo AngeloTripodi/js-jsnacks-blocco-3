e43d306c640b9a3b416bbe015387bf6b63d8053f
AngeloTripodi <tripodi.angelo@gmail.com>
2022-11-28 16:50:18 +0100

    push on array

--- js/script_2.js
+++ js/script_2.js
@@ -4,5 +4,7 @@ let sum = 0;
 while (sum < 50){
     number = parseInt(prompt('Inserisci un numero'));
     sum = sum + number;
+    listNumbers.push(number);
 }
 console.log(sum);
+console.log(listNumbers);

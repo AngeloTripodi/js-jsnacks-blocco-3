7602b918bbc5d9f99266deb47e7b2e3278424fce
AngeloTripodi <tripodi.angelo@gmail.com>
2022-11-28 17:32:19 +0100

    Add if

--- js/script_3.js
+++ js/script_3.js
@@ -1,7 +1,15 @@
-const array1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-const array2 = [2, 0, 3, 0, 3, 0, 0, 8, 99, 0, 18, 189, 42, 0, 33, 44, 88, 99]
+const array1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 44, 88, 66]
+const array2 = [2, 0, 3, 0, 3, 0, 0, 8, 99, 0]
 
-while (array1.length < array2.length) {
-    array1.push(0);
+if (array1.length < array2.length) {
+    while (array1.length < array2.length) {
+        array1.push(0);
+    }
+}
+
+if (array2.length < array1.length) {
+    while (array2.length < array1.length) {
+        array2.push(0);
+    }
 }
 console.log(array1, array2);

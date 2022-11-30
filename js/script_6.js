e7f3ab8403e10c330a53102f0771afaac3d99788
AngeloTripodi <tripodi.angelo@gmail.com>
2022-11-30 11:45:14 +0100

    add more element of array

--- js/script_6.js
+++ js/script_6.js
@@ -7,11 +7,11 @@ function createNewElementP(array,[i]){
     return newElement;
 }
 
-const array = [1,33,55,64,33,22,10,44];
+const array = [1,33,55,64,33,22,10,44,88, 22, 7];
 
 for (let i=0; i<array.length; i++){
     if (array[i] % 2 === 0){
-        divElementGreen.appendChild(createNewElementP(array, [i]));
+        divElementGreen.appendChild(createNewElementP(array,[i]));
     }
     else {
         divElementRed.appendChild(createNewElementP(array,[i]));

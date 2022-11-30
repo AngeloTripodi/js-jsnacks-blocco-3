837024ff908b86b587be61941ac1f88835934cc3
AngeloTripodi <tripodi.angelo@gmail.com>
2022-11-30 11:44:22 +0100

    Add script_6

--- js/Script_4.js
+++ js/Script_4.js
@@ -3,14 +3,14 @@ function getRandomNumber (num1, num2) {
     return randomNumbers;
 }
 
-let Result = getRandomNumber(0,10);
-console.log(Result);
+// let Result = getRandomNumber(0,10);
+// console.log(Result);
 
-// for (i = 0; i<2; i++) 
-// {
-// let getResultRandomNumber = parseInt(prompt("insrisci un numero"));
-// console.log(getRandomNumber(getResultRandomNumber, getResultRandomNumber));
-// }
 
+let numero1 = parseInt(prompt("insrisci un numero"));
+let numero2 = parseInt(prompt("insrisci un numero"));
+let getResultRandomNumber = getRandomNumber(numero1, numero2);
+
+console.log(getResultRandomNumber);
 
 
---
+++ js/script_6.js
@@ -0,0 +1,19 @@
+const divElementGreen = document.getElementById('green');
+const divElementRed = document.getElementById('red');
+
+function createNewElementP(array,[i]){
+    const newElement = document.createElement('p');
+    newElement.innerText = array[i];
+    return newElement;
+}
+
+const array = [1,33,55,64,33,22,10,44];
+
+for (let i=0; i<array.length; i++){
+    if (array[i] % 2 === 0){
+        divElementGreen.appendChild(createNewElementP(array, [i]));
+    }
+    else {
+        divElementRed.appendChild(createNewElementP(array,[i]));
+    }
+}
